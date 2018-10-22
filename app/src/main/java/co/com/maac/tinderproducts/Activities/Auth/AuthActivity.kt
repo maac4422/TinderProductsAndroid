6abe7fb95afb49393f9d5b9a4b8978e4be3fee28
miguel.arenas <miguelarenas98@hotmail.com>
2018-10-22 16:01:51 -0500

    Create a base activity

--- app/src/main/java/co/com/maac/tinderproducts/Activities/Auth/AuthActivity.kt
+++ app/src/main/java/co/com/maac/tinderproducts/Activities/Auth/AuthActivity.kt
@@ -2,12 +2,15 @@ package co.com.maac.tinderproducts.Activities.Auth
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import co.com.maac.tinderproducts.Activities.Global.BaseActivity
 import co.com.maac.tinderproducts.R
 
-class AuthActivity : AppCompatActivity() {
+class AuthActivity : BaseActivity() {
+
+    override val layoutRes = R.layout.activity_auth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+        //setContentView(R.layout.activity_auth)
     }
 }
--- app/src/main/java/co/com/maac/tinderproducts/Activities/Global/BaseActivity.kt
+++ app/src/main/java/co/com/maac/tinderproducts/Activities/Global/BaseActivity.kt
@@ -1,7 +1,31 @@
 package co.com.maac.tinderproducts.Activities.Global
 
+import android.os.Bundle
 import com.arellomobile.mvp.MvpAppCompatActivity
 import com.arellomobile.mvp.MvpView
 import com.arellomobile.mvp.presenter.InjectPresenter
 
-class BaseActivity {}
+abstract class BaseActivity : MvpAppCompatActivity() {
+    abstract  val layoutRes: Int
+    private var instanceStateSaved: Boolean = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layoutRes)
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+    }
+
+
+
+}
